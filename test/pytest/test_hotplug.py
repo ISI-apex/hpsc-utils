81dd133df920fa9fc3dda1e45aad4e2ba6a22452
David Chen <chen749@localhost.localdomain>
2020-02-06 15:05:03 -0800

    add mmu test wrapper

--- test/pytest/test_hotplug.py
+++ test/pytest/test_hotplug.py
@@ -3,16 +3,15 @@ import pytest
 from pexpect.fdpexpect import fdspawn
 
 @pytest.mark.timeout(400)
-
-def test_hotplug(qemu_instance_per_fcn):
-    qemu_instance_per_fcn['serial2'].sendline('root')
+@pytest.mark.parametrize('core_num', range(1,8))
+def test_hotplug(qemu_instance_per_mdl, core_num):
+    qemu_instance_per_mdl['serial2'].sendline('root')
     cpu_path = '/sys/devices/system/cpu/'
 
-    for i in range(1,8):
-        qemu_instance_per_fcn['serial2'].sendline('echo 0 > ' + cpu_path + 'cpu' + str(i) + '/online') #turn CPU i off
-        qemu_instance_per_fcn['serial2'].sendline('more ' + cpu_path + 'offline')
-        assert(qemu_instance_per_fcn['serial2'].expect(str(i)) == 0)
+    qemu_instance_per_mdl['serial2'].sendline('echo 0 > ' + cpu_path + 'cpu' + str(core_num) + '/online') #turn CPU core_num off
+    qemu_instance_per_mdl['serial2'].sendline('more ' + cpu_path + 'offline')
+    assert(qemu_instance_per_mdl['serial2'].expect(str(core_num)) == 0)
 
-        qemu_instance_per_fcn['serial2'].sendline('echo 1 > ' + cpu_path + 'cpu' + str(i) + '/online') #turn CPU i on
-        qemu_instance_per_fcn['serial2'].sendline('more ' + cpu_path + 'online')
-        assert(qemu_instance_per_fcn['serial2'].expect('0-7') == 0)
+    qemu_instance_per_mdl['serial2'].sendline('echo 1 > ' + cpu_path + 'cpu' + str(core_num) + '/online') #turn CPU core_num on
+    qemu_instance_per_mdl['serial2'].sendline('more ' + cpu_path + 'online')
+    assert(qemu_instance_per_mdl['serial2'].expect('0-7') == 0)
--- test/pytest/test_mbox_multi_system.py
+++ test/pytest/test_mbox_multi_system.py
@@ -5,18 +5,16 @@ from pexpect.fdpexpect import fdspawn
 
 @pytest.mark.timeout(400)
 @pytest.mark.parametrize('core_num', range(8))
-
-def test_rtps_hpps(qemu_instance_per_fcn, host, core_num):
-    hostname = 'hpscqemu'
+def test_rtps_hpps(qemu_instance_per_mdl, host, core_num):
     tester_remote_path = '/opt/hpsc-utils/mbox-server-tester'
     
-    qemu_instance_per_fcn['serial1'].sendline('test_mbox_rtps_hpps 3 4')
-    assert(qemu_instance_per_fcn['serial1'].expect('TEST: test_mbox_rtps_hpps: begin') == 0)
+    qemu_instance_per_mdl['serial1'].sendline('test_mbox_rtps_hpps 3 4')
+    assert(qemu_instance_per_mdl['serial1'].expect('TEST: test_mbox_rtps_hpps: begin') == 0)
 
-    out = subprocess.run(['ssh', hostname] + [tester_remote_path] + ['-c', str(core_num)], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = subprocess.run(['ssh', host] + [tester_remote_path] + ['-c', str(core_num)], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    assert(qemu_instance_per_fcn['serial1'].expect('TEST_LINK: request: ACK received') == 0)
-    assert(qemu_instance_per_fcn['serial1'].expect('TEST_LINK: request: reply received') == 0)
-    assert(qemu_instance_per_fcn['serial1'].expect('TEST: test_mbox_rtps_hpps: success') == 0)
+    assert(qemu_instance_per_mdl['serial1'].expect('TEST_LINK: request: ACK received') == 0)
+    assert(qemu_instance_per_mdl['serial1'].expect('TEST_LINK: request: reply received') == 0)
+    assert(qemu_instance_per_mdl['serial1'].expect('TEST: test_mbox_rtps_hpps: success') == 0)
 
     assert out.returncode == 64 #hpps returns number of bytes sent. we expect 64 bytes
