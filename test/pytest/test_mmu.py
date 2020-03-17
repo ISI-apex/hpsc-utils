9330a7af58f042a7539710b809833f2216842fb1
Alexei Colin <acolin@isi.edu>
2020-03-16 23:27:57 -0400

    pytest: mmu: sync output with hpsc-baremetal

--- test/pytest/test_mmu.py
+++ test/pytest/test_mmu.py
@@ -4,6 +4,6 @@ from pexpect.fdpexpect import fdspawn
 
 @pytest.mark.timeout(400)
 def test_trch_mmu(qemu_instance_per_fcn):
-    assert(qemu_instance_per_fcn['serial0'].expect('MMU map-write-read test success') == 0)
-    assert(qemu_instance_per_fcn['serial0'].expect('MMU write-map-read test success') == 0)
-    assert(qemu_instance_per_fcn['serial0'].expect('MMU mapping swap test success') == 0)
+    assert(qemu_instance_per_fcn['serial0'].expect('TEST: rt mmu: map-write-read: success') == 0)
+    assert(qemu_instance_per_fcn['serial0'].expect('TEST: rt mmu: write-map-read: success') == 0)
+    assert(qemu_instance_per_fcn['serial0'].expect('TEST: rt mmu: mapping swap: success') == 0)
