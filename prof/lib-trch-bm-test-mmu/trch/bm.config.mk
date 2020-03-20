67253859f76795ec1a8d539655c43802a31d57ce
Alexei Colin <acolin@isi.edu>
2020-03-20 00:46:40 -0400

    prof: enable RT MMU tests in TRCH BM
    This is useful for pytest tests that look for the  output of this
    MMU test specifically.

---
+++ prof/lib-trch-bm-test-mmu/trch/bm.config.mk
@@ -0,0 +1,4 @@
+TEST_RT_MMU                                     = 1
+TEST_RT_MMU_BASIC                               = 1
+TEST_RT_MMU_32_ACCESS_PHYSICAL  		= 0 # currently fails
+TEST_RT_MMU_MAPPING_SWAP                	= 1 # incompatible with RTPS's TEST_RT_MMU
