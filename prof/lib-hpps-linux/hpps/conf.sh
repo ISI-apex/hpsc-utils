b9149f1f1732a5b1455ad968d617b8d3deaddae2
Alexei Colin <acolin@isi.edu>
2020-03-20 00:32:48 -0400

    prof: linux: mv kernel to make space for initramfs

--- prof/lib-hpps-linux/hpps/conf.sh
+++ prof/lib-hpps-linux/hpps/conf.sh
@@ -1,3 +1,3 @@
 # Some params needed by the build but not part of any memory map file
 
-KERN_LOAD_ADDR=0x81a80000 # (base + TEXT_OFFSET), where base must be aligned to 2MB
+KERN_LOAD_ADDR=0x82080000 # (base + TEXT_OFFSET), where base must be aligned to 2MB
